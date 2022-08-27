aed17ab05067ae8830ac0cd1e975ee2d0f939230
4lexanderKostadinov <111220879+4lexanderKostadinov@users.noreply.github.com>
2022-08-27 12:02:37 +0300

    Beta 0.4.2

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const menu = document.querySelector('#mobile-menu'); 
 const menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', function()) {
+    menu.classList.toggle('is-active');
+    menu.classList.toggle('active');
+     
+}
